866eda53022f87bf44cd4b5f76f5bf6c72529159
EimantasJaskonis <eimantasjaskonis@gmail.com>
2025-04-17 10:05:15 +0300

    Home start

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Routes, Route } from 'react-router';
+import Home from './components/pages/Home';
+
 const App = () => {
 
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
